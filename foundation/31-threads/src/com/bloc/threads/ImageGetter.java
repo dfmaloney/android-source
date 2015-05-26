9ef290d83c29c5e1c02b2fda6cda7cf4bd686792
Drew Maloney <mr.drew.maloney@gmail.com>
2015-05-26 14:47:16 -0500

    checkpoint 31 completed

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,5 +1,10 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
 
 	/*
@@ -19,6 +24,13 @@ public class ImageGetter extends Thread {
  	 *	behavior as described above.
 	/************************************************/
 
+	String webOrigin = "";
+	boolean shouldOpen = false;
+	ImageGetter(String url, boolean validity){
+		webOrigin = url;
+		shouldOpen = validity;
+	}
+
 	@Override
 	public void run() {
 		/************************************************
@@ -26,5 +38,32 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+		new Thread(){
+			public void run(){
+			try {
+				File existingImage = new File("google_logo.png");
+				if (existingImage.exists()) {
+					existingImage.delete();
+				}
+				URL url = new URL(webOrigin);
+				BufferedImage bufferedImage = ImageIO.read(url);
+				File outputfile = new File("google_logo.png");
+				ImageIO.write(bufferedImage, "png", outputfile);
+				if (shouldOpen){
+					if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+						Runtime.getRuntime().exec("open google_logo.png");
+					} else {
+						Runtime.getRuntime().exec("google_logo.png");
+					}
+					}
+			} catch (IOException e) {
+						e.printStackTrace();
+					System.exit(1);
+			} catch (Exception e) {
+					e.printStackTrace();
+					System.exit(1);
+			}
+		}
+		};
 	}
 }
